8cfb475fc50ab1dae0657aa24de632c19a636a30
Alexis <apovedaf0221@gmail.com>
2025-08-12 21:27:41 -0400

    Remove private_key var and use private_key_path

--- main.tf
+++ main.tf
@@ -6,17 +6,20 @@ terraform {
   }
 }
 
-variable "remote_host" {}
-variable "remote_user" {}
-variable "private_key" {}
+# Variables que sí usamos desde el workflow
+variable "remote_host"      { type = string }
+variable "remote_user"      { type = string }
+variable "private_key_path" { type = string }
 
 resource "null_resource" "deploy_app" {
   provisioner "remote-exec" {
     connection {
       host        = var.remote_host
       user        = var.remote_user
-      private_key = var.private_key
+      # Lee la clave desde el fichero proporcionado por el workflow
+      private_key = file(var.private_key_path)
     }
+
     inline = [
       "sudo mkdir -p /var/www/devsecops",
       "echo '<h1>Provisioned by Terraform</h1>' | sudo tee /var/www/devsecops/index.html"
